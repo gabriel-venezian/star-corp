6fc7f8b2da7bcabfd7d7581dce45ae51dd3647cd
Gabriel Venezian <gabriel.venezian@hotmail.com>
2022-05-15 23:53:21 -0300

    Add API request methods (Address)

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,19 @@
 var myApp = angular.module("myApp", []);
 
 myApp.controller("myController", function ($scope, $http) {
-  // Formatar Data
-  function formatarData(data) {
-    let d = new Date(data),
-      mes = "" + (d.getMonth() + 1),
-      dia = "" + d.getDate(),
-      ano = d.getFullYear();
-
-    if (mes.length < 2) mes = "0" + mes;
-    if (dia.length < 2) dia = "0" + dia;
-
-    return [dia, mes, ano].join("/");
-  }
-
-  // Listar pessoas
-  $scope.pessoas = [];
-
-  const url = "https://www.selida.com.br/avaliacaotecnica/api";
-  const headers = {
+  const URL = "https://www.selida.com.br/avaliacaotecnica/api";
+  const HEADERS = {
     headers: {
-      accept: "text/plain",
       chave: "",
     },
   };
+  // Pessoas
+  $scope.pessoas = [];
 
-  let endpoint = "Pessoas/GetAll";
+  $scope.getPessoas = function () {
+    let endpoint = "Pessoas/GetAll";
 
-  $http.get(`${url}/${endpoint}`, headers).then((resposta) => {
-    if (resposta.status === 200) {
+    $http.get(`${URL}/${endpoint}`, HEADERS).then((resposta) => {
       let pessoas = resposta.data["data"];
 
       for (let i = 0; i < pessoas.length; i++) {
@@ -43,14 +28,13 @@ myApp.controller("myController", function ($scope, $http) {
           telefone: pessoa["telefone"],
           celular: pessoa["celular"],
         };
-
         $scope.pessoas.push(dadosPessoa);
       }
-    }
-  });
+    });
+  };
 
-  // Adicionar pessoa
-  $scope.adicionarPessoa = function () {
+  // POST pessoa
+  $scope.postPessoa = function () {
     let pessoa = $scope.novaPessoa;
 
     let dadosParaEnvio = JSON.stringify({
@@ -65,16 +49,15 @@ myApp.controller("myController", function ($scope, $http) {
     endpoint = "Pessoas/";
 
     $http
-      .post(`${url}/${endpoint}`, dadosParaEnvio, headers)
+      .post(`${URL}/${endpoint}`, dadosParaEnvio, HEADERS)
       .then((resposta) => {
         if (resposta.status === 200) {
-          alert("Usuário cadastrado com sucesso");
-          document.location.reload();
+          $scope.postEndereco();
         }
       });
   };
 
-  // Selecionar inputs
+  // Selecionar inputs pessoa
   let _inputs = {
     nome: document.getElementById("editarNome"),
     dataNascimento: document.getElementById("editarDataNascimento"),
@@ -86,6 +69,7 @@ myApp.controller("myController", function ($scope, $http) {
 
   // Selecionar pessoa
   $scope.selecionarPessoa = function (pessoa) {
+    $scope.reiniciarScopeEnderecos();
     $scope.pessoaSelecionada = pessoa;
 
     let dataInput = String(pessoa["dataNascimento"]);
@@ -97,6 +81,105 @@ myApp.controller("myController", function ($scope, $http) {
     _inputs.email.value = pessoa["email"];
     _inputs.telefone.value = pessoa["telefone"];
     _inputs.celular.value = pessoa["celular"];
+
+    let pessoaId = pessoa["pessoaId"];
+
+    endpoint = `Endereco/GetAll/${pessoaId}`;
+
+    $http.get(`${URL}/${endpoint}`, HEADERS).then((resposta) => {
+      let enderecos = resposta.data["data"];
+
+      for (let endereco in enderecos) {
+        enderecos[endereco]["cidadeUf"] =
+          enderecos[endereco]["cidade"] + " - " + enderecos[endereco]["uf"];
+      }
+
+      $scope.enderecos = enderecos;
+    });
+  };
+
+  // Endereços
+  $scope.enderecos = [];
+
+  // Salvar endereço
+  $scope.salvarEndereco = function () {
+    let endereco = $scope.novoEndereco;
+
+    if (
+      endereco !== undefined &&
+      endereco["logradouro"] !== undefined &&
+      endereco["numero"] !== undefined &&
+      endereco["bairro"] !== undefined &&
+      endereco["cidade"] !== undefined &&
+      endereco["uf"] !== undefined
+    ) {
+      let cidadeUf = endereco["cidade"] + " - " + endereco["uf"];
+
+      let dadosEndereco = {
+        logradouro: endereco["logradouro"],
+        numero: endereco["numero"],
+        bairro: endereco["bairro"],
+        cidade: endereco["cidade"],
+        uf: endereco["uf"],
+        cidadeUf: cidadeUf,
+      };
+
+      $scope.novoEnderecoAdicionado = [];
+      $scope.enderecos.push(dadosEndereco);
+      $scope.novoEnderecoAdicionado.push(dadosEndereco);
+
+      $scope.novoEndereco = [];
+      salvarModalNovaPessoa.disabled = false;
+    } else {
+      alert("Por favor, insira as informaçoes de endereço corretamente.");
+    }
+  };
+
+  $scope.postEndereco = function () {
+    let endpoint = "Pessoas/GetAll";
+
+    $http.get(`${URL}/${endpoint}`, HEADERS).then((resposta) => {
+      let pessoas = resposta.data["data"];
+      let idAtual = pessoas[pessoas.length - 1]["pessoaId"];
+
+      endpoint = "Endereco";
+
+      for (endereco in $scope.enderecos) {
+        let dadoEndereco = $scope.enderecos[endereco];
+
+        let enderecoParaEnvio = JSON.stringify({
+          pessoaId: idAtual,
+          logradouro: dadoEndereco["logradouro"],
+          numero: dadoEndereco["numero"],
+          bairro: dadoEndereco["bairro"],
+          cidade: dadoEndereco["cidade"],
+          uf: dadoEndereco["uf"],
+        });
+
+        $http.post(`${URL}/${endpoint}`, enderecoParaEnvio, HEADERS);
+      }
+    });
+  };
+
+  // Deletar endereço
+  $scope.deletarEndereco = function (endereco) {
+    $scope.enderecos.splice($scope.enderecos.indexOf(endereco), 1);
+  };
+
+  // Deletar endereço da API
+  $scope.deletarEnderecoApi = function (endereco) {
+    let enderecoId = endereco["enderecoId"];
+
+    endpoint = `Endereco/${enderecoId}`;
+
+    $http.delete(`${URL}/${endpoint}`, HEADERS).then((resposta) => {
+      if (resposta.status === 202) {
+        alert("Endereço apagado com sucesso");
+        $scope.deletarEndereco(endereco);
+      }
+    });
+
+    $scope.deletarEndereco();
   };
 
   // Deletar pessoa
@@ -104,7 +187,7 @@ myApp.controller("myController", function ($scope, $http) {
     let pessoaId = $scope.pessoaSelecionada["pessoaId"];
     endpoint = `Pessoas/${pessoaId}`;
 
-    $http.delete(`${url}/${endpoint}`, headers).then((resposta) => {
+    $http.delete(`${URL}/${endpoint}`, HEADERS).then((resposta) => {
       if (resposta.status === 202) {
         alert("Registro apagado com sucesso.");
         document.location.reload();
@@ -114,6 +197,19 @@ myApp.controller("myController", function ($scope, $http) {
 
   // Editar pessoa
   $scope.editarPessoa = function () {
+    // Verificar se o registro atual contém alterações para definir se será necessário realizar o put ou não
+    let dataNascimento = String($scope.pessoaSelecionada["dataNascimento"]);
+    dataNascimento = dataNascimento.split("/").reverse().join("-");
+
+    let pessoaSelecionada = JSON.stringify({
+      nome: $scope.pessoaSelecionada["nome"],
+      dataNascimento: dataNascimento,
+      idade: Number($scope.pessoaSelecionada["idade"]),
+      email: $scope.pessoaSelecionada["email"],
+      telefone: $scope.pessoaSelecionada["telefone"],
+      celular: $scope.pessoaSelecionada["celular"],
+    });
+
     let dadosParaEnvio = JSON.stringify({
       nome: _inputs.nome.value,
       dataNascimento: _inputs.dataNascimento.value,
@@ -123,16 +219,50 @@ myApp.controller("myController", function ($scope, $http) {
       celular: _inputs.celular.value,
     });
 
-    let pessoaId = $scope.pessoaSelecionada["pessoaId"];
-    endpoint = `Pessoas/${pessoaId}`;
+    if (pessoaSelecionada !== dadosParaEnvio) {
+      let pessoaId = $scope.pessoaSelecionada["pessoaId"];
+      endpoint = `Pessoas/${pessoaId}`;
+      $http.put(`${URL}/${endpoint}`, dadosParaEnvio, HEADERS);
+    }
 
-    $http
-      .put(`${url}/${endpoint}`, dadosParaEnvio, headers)
-      .then((resposta) => {
-        if (resposta.status === 200) {
-          alert("Registro alterado com sucesso.");
-          location.reload();
-        }
-      });
+    if ($scope.novoEnderecoAdicionado !== []) {
+      let pessoaId = $scope.pessoaSelecionada["pessoaId"];
+
+      endpoint = "Endereco";
+
+      for (endereco in $scope.novoEnderecoAdicionado) {
+        let dadoEndereco = $scope.novoEnderecoAdicionado[endereco];
+
+        let enderecoParaEnvio = JSON.stringify({
+          pessoaId: pessoaId,
+          logradouro: dadoEndereco["logradouro"],
+          numero: dadoEndereco["numero"],
+          bairro: dadoEndereco["bairro"],
+          cidade: dadoEndereco["cidade"],
+          uf: dadoEndereco["uf"],
+        });
+
+        $http.post(`${URL}/${endpoint}`, enderecoParaEnvio, HEADERS);
+      }
+    }
   };
+
+  $scope.reiniciarScopeEnderecos = function () {
+    $scope.enderecos = [];
+    $scope.novoEndereco = [];
+    $scope.novoEnderecoAdicionado = [];
+  };
+
+  // Formatar Data
+  function formatarData(data) {
+    let d = new Date(data),
+      mes = "" + (d.getMonth() + 1),
+      dia = "" + d.getDate(),
+      ano = d.getFullYear();
+
+    if (mes.length < 2) mes = "0" + mes;
+    if (dia.length < 2) dia = "0" + dia;
+
+    return [dia, mes, ano].join("/");
+  }
 });
